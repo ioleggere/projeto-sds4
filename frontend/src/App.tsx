192ad351aecef5bd95846827e0e2e016729f95ff
Euler Lima <euler410@gmail.com>
2021-09-06 11:43:56 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,16 @@
+import NavBar from "components/Navbar";
+import Footer from "components/Footer";
+import DataTable from "components/DataTable";
 function App() {
   return (
-    <div>
-      <h1 className = "text-primary">Hello World!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className = "container">
+        <h1 className="text-primary">Hello World!</h1>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
